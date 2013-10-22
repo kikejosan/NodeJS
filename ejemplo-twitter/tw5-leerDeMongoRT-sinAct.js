6aa8767f29d2760549449165e7e53fbfa0e85ecb
jgallud <jose.gallud@uclm.es>
2013-10-22 22:52:11 +0200

    claves twitter

--- ejemplo-twitter/tw5-leerDeMongoRT-sinAct.js
+++ ejemplo-twitter/tw5-leerDeMongoRT-sinAct.js
@@ -1,10 +1,10 @@
 //Conectar a Twitter con el API de Twitter
 var	twitter=require("ntwitter");
 var twit=new twitter({
-	consumer_key:"ieOAbjJALLbJItedFGSnXA",
-	consumer_secret:"tNjmERYSjqcPfbn0VPOf2XQzlRdI3FgLqO3BbZcu7w",
-	access_token_key:"258725479-KUiuNk8H7MhrAMS64HvPQuTKevAYjgIkzDSlQT0b",
-	access_token_secret:"yrXFkithnd3qywGynx4XWQYLo21s4Xhjqma65gaNws"
+	consumer_key:"----",
+	consumer_secret:"-----",
+	access_token_key:"----",
+	access_token_secret:"---"
 });
 
 var fs=require("fs");
